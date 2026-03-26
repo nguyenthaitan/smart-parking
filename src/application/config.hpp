78d795bb39796927ad6ef0f76236cb2a1d93b5bc
nguyenthaitan <nguyenthaitan9@gmail.com>
2026-03-26 23:33:10 +0700

    feat: add config file

--- src/application/config.hpp
+++ src/application/config.hpp
@@ -1,4 +1,20 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <string>
+#include "../enums.hpp"
+
+struct SimulationEvent {
+    int timestamp_seconds;
+    std::string vehicle_id;
+    EventType event_type;
+    VehicleType vehicle_type;
+    int duration_minutes;
+};
+
+struct ApplicationConfig {
+    std::string parking_config_file;
+    std::string simulation_events_file;
+};
+
 #endif // CONFIG_HPP
