f87e2237ac1e435abb1d29545b41d2de50993445
nguyenthaitan <nguyenthaitan9@gmail.com>
2026-03-26 23:14:13 +0700

    feat: add enum type

---
+++ src/enum.hpp
@@ -0,0 +1,16 @@
+#ifndef ENUMS_HPP
+#define ENUMS_HPP
+
+// Vehicle type enumeration
+enum class VehicleType {
+    MOTORCYCLE = 0,
+    CAR = 1
+};
+
+// Event type enumeration
+enum class EventType {
+    ARRIVAL = 0,
+    DEPARTURE = 1
+};
+
+#endif // ENUMS_HPP
