376185873b06ac2954dd9c176fd1e48eca90eec3
Jumper <dqiznh24@gmail.com>
2026-03-27 20:45:43 +0700

    Add SimulationStatistics struct to config.hpp
    Added SimulationStatistics struct for tracking simulation metrics.

--- src/application/config.hpp
+++ src/application/config.hpp
@@ -2,6 +2,8 @@
 #define CONFIG_HPP
 
 #include <string>
+#include <vector>
+#include <chrono>
 #include "../enums.hpp"
 
 struct SimulationEvent {
@@ -12,9 +14,24 @@ struct SimulationEvent {
     int duration_minutes;
 };
 
+struct SimulationStatistics {
+    int total_arrivals;
+    int total_departures;
+    int total_allocations;
+    int total_rejections;
+    int motorcycle_rejections;
+    int car_rejections;
+    int current_parked_count;
+    double current_occupancy_rate;
+    double average_parking_duration;
+    int peak_occupancy;
+};
+
 struct ApplicationConfig {
     std::string parking_config_file;
     std::string simulation_events_file;
+    std::string output_file;
+    bool verbose;
 };
 
 #endif // CONFIG_HPP
